3e94dea73006b2eaabbe93c5cf5c62785bfc575b
ardit <ardit_dine@hotmail.com>
2020-11-28 20:51:17 +0800

    add retry and timeout

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ io.on("connection", async(socket) => {
 
 async function fetchData() {
   try {
-    const res = await request("https://www.airvisual.com/api/v3/node/5e09686cff5ce29bfeb6f955");
+    const res = await request("https://www.airvisual.com/api/v3/node/5e09686cff5ce29bfeb6f955").retry(3).timeout(10000);
     const body = res.body || {};
     const { label } = aqiCalculator(body.current_measurement.p2.conc)
     return {...body.current_measurement, label};
