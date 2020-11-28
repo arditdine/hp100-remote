6a56d8257c7836ba65a3652f328283ebaa252575
ardit <ardit_dine@hotmail.com>
2020-11-28 20:54:52 +0800

    add index.html

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/index.html", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
 const users = {};
 
 io.on("connection", async(socket) => {
